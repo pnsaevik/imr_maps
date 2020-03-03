6fb05ede59797fc9c69f5b1309ef9b6bfe34454b
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2020-03-03 13:45:10 +0100

    Rename module

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,19 +1,19 @@
-import imr.farms
+from imr.farms import fiskdir
 import pytest
 
 
 @pytest.fixture(scope='module')
 def fiskdir_wfs():
-    return imr.farms.get_fiskdir_wfs()
+    return fiskdir.get_fiskdir_wfs()
 
 
 class Test_get_layer:
     def test_returns_layer_name_if_title_match(self, fiskdir_wfs):
         title = 'Akvakultur - lokaliteter'
-        layer = imr.farms.get_layer(title, fiskdir_wfs)
+        layer = fiskdir.get_layer(title, fiskdir_wfs)
         assert layer == 'layer_262'
 
     def test_returns_none_if_no_match(self, fiskdir_wfs):
         title = 'No title'
-        layer = imr.farms.get_layer(title, fiskdir_wfs)
+        layer = fiskdir.get_layer(title, fiskdir_wfs)
         assert layer is None

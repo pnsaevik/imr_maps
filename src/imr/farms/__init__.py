5c1d534eebc364af22af108aebc99f19d22fc26a
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2020-03-03 13:43:37 +0100

    Find layer name from title

--- src/imr/farms/__init__.py
+++ src/imr/farms/__init__.py
@@ -1,7 +1,7 @@
 from osgeo import ogr, gdal
 
 
-def get_dataset():
+def get_fiskdir_wfs():
     # Speeds up querying WFS capabilities for services with alot of layers
     gdal.SetConfigOption('OGR_WFS_LOAD_MULTIPLE_LAYER_DEFN', 'NO')
 
@@ -13,16 +13,16 @@ def get_dataset():
     url = 'https://ogc.fiskeridir.no/wfs.ashx'
     wfs_drv = ogr.GetDriverByName('WFS')
     wfs_ds = wfs_drv.Open('WFS:' + url)
-
     if not wfs_ds:
         raise IOError(f'Can not open WFS datasource: {url}')
+    return wfs_ds
 
+
+def get_layer(title, wfs_ds):
     # Iterate over layers
-    # layers = {}
-    # for i in range(wfs_ds.GetLayerCount()):
-    #    layer = wfs_ds.GetLayerByIndex(i)
-    return wfs_ds.GetLayerCount()
+    for i in range(wfs_ds.GetLayerCount()):
+        layer = wfs_ds.GetLayerByIndex(i)
+        if title == layer.GetMetadataItem('TITLE'):
+            return layer.GetDescription()
 
-
-def main():
-    return get_dataset()
+    return None
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,19 @@
 import imr.farms
+import pytest
 
 
-def test_dummy():
-    assert imr.farms.main() == 55
+@pytest.fixture(scope='module')
+def fiskdir_wfs():
+    return imr.farms.get_fiskdir_wfs()
+
+
+class Test_get_layer:
+    def test_returns_layer_name_if_title_match(self, fiskdir_wfs):
+        title = 'Akvakultur - lokaliteter'
+        layer = imr.farms.get_layer(title, fiskdir_wfs)
+        assert layer == 'layer_262'
+
+    def test_returns_none_if_no_match(self, fiskdir_wfs):
+        title = 'No title'
+        layer = imr.farms.get_layer(title, fiskdir_wfs)
+        assert layer is None

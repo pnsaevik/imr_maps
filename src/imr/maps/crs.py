7e8c993e4ed096b89e68893e25118c306caa77ff
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2020-03-05 08:40:43 +0100

    Add common epsg codes

--- src/imr/maps/crs.py
+++ src/imr/maps/crs.py
@@ -1,6 +1,28 @@
 from osgeo import osr
 
 
+EPSG_CODES = dict(
+    wgs84=4326,
+    utm29n=32629,
+    utm30n=32630,
+    utm31n=32631,
+    utm32n=32632,
+    utm33n=32633,
+    utm34n=32634,
+    utm35n=32635,
+    utm36n=32636,
+    etrs89=4258,
+    etrs89_utm29n=25829,
+    etrs89_utm30n=25830,
+    etrs89_utm31n=25831,
+    etrs89_utm32n=25832,
+    etrs89_utm33n=25833,
+    etrs89_utm34n=25834,
+    etrs89_utm35n=25835,
+    etrs89_utm36n=25836,
+)
+
+
 def projection_from_dataset(dset):
     potential_projections = [(k, v) for k, v in dset.data_vars.items()
                              if v.shape == () and 'spatial_ref' in v.attrs]

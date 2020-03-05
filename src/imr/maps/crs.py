8045fdedec6b7bb95529df26c115157d0e4cfce7
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2020-03-05 09:06:56 +0100

    Add norkyst800 transforms

--- src/imr/maps/crs.py
+++ src/imr/maps/crs.py
@@ -74,6 +74,58 @@ def projection_local(lon, lat):
     return ref
 
 
+def projection_nf160(named_area, metric_unit=False):
+    params = {
+        'A01': (18704, 10752, 160, 70, 'NF160_A01'),
+        'A02': (19254, 10352, 160, 70, 'NF160_A02'),
+        'A03': (19254, 9282, 160, 70, 'NF160_A03'),
+        'A04': (18854, 8992, 160, 70, 'NF160_A04'),
+        'A05': (18104, 8922, 160, 70, 'NF160_A05'),
+        'A06': (16904, 8582, 160, 70, 'NF160_A06'),
+        'A07': (16074, 9082, 160, 70, 'NF160_A07'),
+        'A08': (14804, 8932, 160, 70, 'NF160_A08'),
+        'A09': (14054, 8932, 160, 70, 'NF160_A09'),
+        'A10': (12954, 8802, 160, 70, 'NF160_A10'),
+        'A11': (11554, 8952, 160, 70, 'NF160_A11'),
+        'A12': (10004, 9422, 160, 70, 'NF160_A12'),
+        'A13': (8754, 10552, 160, 70, 'NF160_A13'),
+    }
+
+    return projection_nk800(*params[named_area], metric_unit=metric_unit)
+
+
+def projection_nk800(xp=3991, yp=2230, dx=800, ylon=70, name='NK800', metric_unit=False):
+    if metric_unit:
+        unit_str = 'UNIT["metre",1,AUTHORITY["EPSG","9001"]]'
+        dx_unit = dx
+    else:
+        unit_str = f'UNIT["Cells",{dx}]]'
+        dx_unit = 1
+
+    wkt = f"""PROJCS["{name}",
+        GEOGCS["ETRS89",
+            DATUM["European_Terrestrial_Reference_System_1989",
+                SPHEROID["GRS 1980",6378137,298.257222101,
+                    AUTHORITY["EPSG","7019"]],
+                AUTHORITY["EPSG","6258"]],
+            PRIMEM["Greenwich",0,
+                AUTHORITY["EPSG","8901"]],
+            UNIT["degree",0.01745329251994328,
+                AUTHORITY["EPSG","9122"]],
+            AUTHORITY["EPSG","4258"]],
+        PROJECTION["Polar_Stereographic"],
+        PARAMETER["latitude_of_origin",60],
+        PARAMETER["central_meridian",{ylon}],
+        PARAMETER["scale_factor",1],
+        PARAMETER["false_easting",{xp * dx_unit}],
+        PARAMETER["false_northing",{yp * dx_unit}],
+        {unit_str}"""
+
+    ref = osr.SpatialReference()
+    ref.ImportFromWkt(wkt)
+    return ref
+
+
 def transform(lon, lat, from_crs, to_crs):
     if len(lat) == 0 and len(lon) == 0:
         return np.array([lat, lon])
--- tests/test_imr/test_maps.py
+++ tests/test_imr/test_maps.py
@@ -89,3 +89,61 @@ class Test_transform:
         nlon, nlat = crs.transform([lon + 1/60], [lat], wgs84, local)
         assert np.abs(nlon - 930) < 1
         assert np.abs(nlat) < 1
+
+    def test_nk800_to_wgs84(self):
+        xi = [0, 1000, 1000, 0]
+        eta = [0, 0, 500, 500]
+        wgs84 = crs.projection_from_epsg(4326)
+        nk800 = crs.projection_nk800()
+        lon, lat = crs.transform(xi, eta, nk800, wgs84)
+        assert np.all(np.isclose(
+            lat, [55.90836993, 61.91497602, 63.91826663, 57.47693917]))
+        assert np.all(np.isclose(
+            lon, [9.19459011, 16.70710853, 10.04516874, 3.43554155]))
+
+    def test_wgs84_to_nk800(self):
+        lat = [55.90836993, 61.91497602, 63.91826663, 57.47693917]
+        lon = [9.19459011, 16.70710853, 10.04516874, 3.43554155]
+        wgs84 = crs.projection_from_epsg(4326)
+        nk800 = crs.projection_nk800()
+        xi, eta = crs.transform(lon, lat, wgs84, nk800)
+        assert np.all(np.isclose(xi, [0, 1000, 1000, 0], atol=1e-5))
+        assert np.all(np.isclose(eta, [0, 0, 500, 500], atol=1e-5))
+
+    def test_nf160_a01_to_wgs84(self):
+        xi = [0, 1000, 1000, 0]
+        eta = [0, 0, 500, 500]
+        nf160_A01 = crs.projection_nf160('A01')
+        wgs84 = crs.projection_from_epsg(4326)
+        lon, lat = crs.transform(xi, eta, nf160_A01, wgs84)
+        assert np.all(np.isclose(
+            lat, [57.72536405, 58.95227724, 59.31714908, 58.07345413]))
+        assert np.all(np.isclose(
+            lon, [9.89245542, 11.27116288, 10.0741957,  8.72794269]))
+
+    def test_nf160_wgs84_to_nf160_a01(self):
+        lat = [57.72536405, 58.95227724, 59.31714908, 58.07345413]
+        lon = [9.89245542, 11.27116288, 10.0741957,  8.72794269]
+        nf160_A01 = crs.projection_nf160('A01')
+        wgs84 = crs.projection_from_epsg(4326)
+        xi, eta = crs.transform(lon, lat, wgs84, nf160_A01)
+        assert np.all(np.isclose(xi, [0, 1000, 1000, 0], atol=1e-5))
+        assert np.all(np.isclose(eta, [0, 0, 500, 500], atol=1e-5))
+
+    def test_nk800_to_nk800m(self):
+        x = [0, 1, 2]
+        y = [0, 0, 1]
+        nk800m = crs.projection_nk800(metric_unit=True)
+        nk800 = crs.projection_nk800()
+        xm, ym = crs.transform(x, y, nk800, nk800m)
+        assert np.all(np.isclose(xm, [0, 800, 1600], atol=1e-5))
+        assert np.all(np.isclose(ym, [0, 0, 800], atol=1e-5))
+
+    def test_nf160m_to_nf160(self):
+        x = [0, 160, 320]
+        y = [0, 0, 160]
+        nf160_A01m = crs.projection_nf160('A01', metric_unit=True)
+        nf160_A01 = crs.projection_nf160('A01')
+        xm, ym = crs.transform(x, y, nf160_A01m, nf160_A01)
+        assert np.all(np.isclose(xm, [0, 1, 2], atol=1e-5))
+        assert np.all(np.isclose(ym, [0, 0, 1], atol=1e-5))
